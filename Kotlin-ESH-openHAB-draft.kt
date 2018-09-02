cb8f90cb8c347f37f3f920af09b7cc4ff9580610
diyhomeauto <42641251+diyhomeauto@users.noreply.github.com>
2018-09-03 04:50:35 +0530

    Update Kotlin-ESH-openHAB-draft.kt

--- Kotlin-ESH-openHAB-draft.kt
+++ Kotlin-ESH-openHAB-draft.kt
@@ -99,7 +99,7 @@ rule "My Kotlin Rule1" {
     actions {
         // actions go here. free form Kotlin script, with IDE autocomplete,
         // and some nice helpers available in the context
-        val msg = "Intrusion alert, suspicious activity near ${device["Door1"].label}";
+        val msg = "Intrusion alert, suspicious activity near ${device["Door1"].label}"
         // use a predefined function from standard ESH Kotlin extension
         sendUiNotification(msg)
 
